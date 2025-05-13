786294b3ceeedb698a33fc0ae92a9a0e40556c21
kakashi4268 <singhharshpratapsingh227@gmail.com>
2025-05-13 13:38:49 +0530

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,65 @@
+public class SudokuSolver {
+
+    private static final int SIZE = 9;
+
+    public static void main(String[] args) {
+        int[][] board = {
+                { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+                { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+                { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+                { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+                { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+                { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+                { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+                { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+                { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+        };
+
+        if (solve(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+
+    public static boolean solve(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= SIZE; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            board[row][col] = 0; // Backtrack
+                        }
+                    }
+                    return false; // No valid number found
+                }
+            }
+        }
+        return true; // Solved
+    }
+
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == num || board[i][col] == num ||
+                    board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
